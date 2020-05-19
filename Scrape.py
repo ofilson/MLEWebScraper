854129f1fa96db602637eaa728abd9e8c102808a
ofilson <SuperScorer911@gmail.com>
2020-05-19 10:07:58 -0600

    Basic Iterative Structure created

--- Scrape.py
+++ Scrape.py
@@ -13,9 +13,14 @@ driver.get("https://mlesports.gg/matches/") # URL to scrape from: https://mlespo
 content = driver.page_source
 soup = BeautifulSoup(content,features="lxml")
 
-season = soup.find('div', attrs={'id':'elementor-tab-title-2413'}) # Season 8
+#Comment one of these out
+#seasonIDs = ['2413']                #For testing using just one season (season 8)
+seasonIDs = ['2413','2412','2411']  #For testing using all three seasons (8-10)
 
-b = soup.find('div', attrs={'aria-labelledby':'elementor-tab-title-2413'}) #Season 8
-
-c = b.findNext('div', attrs={'class':'sp-template-event-list'})
-print(c.findNext('h4').text)
+for a in seasonIDs:
+    season = soup.find('div', attrs={'id':'elementor-tab-title-'+a}).text # Season number (ie Season 8)
+    print(season)
+    seasonData = soup.find('div', attrs={'aria-labelledby':'elementor-tab-title-'+a}) 
+    for leagueData in seasonData.findAll('div', attrs={'class':'sp-template-event-list'}):
+        League = leagueData.findNext('h4').text
+        print(League)
